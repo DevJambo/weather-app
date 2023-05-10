c455832b5b637149a6300fa0d3e68c9dd6765f27
James Goodman <jamesgoodman206@gmail.com>
2023-05-10 22:17:43 +0100

    added search function

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import getForecast from "./GetForecast";
+import SearchForm from "./SearchForm";
 
 function App() {
   const [forecasts, setForcasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: "" });
   const [selectedDate, setSelectedDate] = useState(0);
+  const [searchText, setSearchText] = useState("");
+
+  const handleCitySearch = () => {
+    getForecast(searchText, setSelectedDate, setForcasts, setLocation);
+  };
 
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate
@@ -16,14 +22,18 @@ function App() {
   const handleForecastSelect = (date) => {
     setSelectedDate(date);
   };
-  useEffect(() => {
-    getForecast(setSelectedDate, setForcasts, setLocation);
-  }, []);
+  // useEffect(() => {
+  //   getForecast(searchText, setSelectedDate, setForcasts, setLocation);
+  // }, [searchText]);
 
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
-
+      <SearchForm
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onSubmit={handleCitySearch}
+      />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
